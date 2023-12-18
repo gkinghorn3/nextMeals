6bcf34f1919a3fa0148e5abd7e13f8dad6f0eae6
Grant <gkinghorn3@icloud.com>
2023-12-18 16:54:37 +0000

    main layout started

---
+++ app/meals/page.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+  return (
+    <>
+      <h1>Meals</h1>
+      <p>
+        <Link href="/meals"></Link>
+      </p>
+      <p>
+        <Link href="/meals"></Link>
+      </p>
+      <p>
+        <Link href="/meals"></Link>
+      </p>
+    </>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,5 @@
+export default function MealsSharePage() {
+    return (
+        <h1>Share Meal</h1>
+    )
+}
